a7a1b9a010661746963403c58d5c756d10eacc42
Nick <NWVS25@proton.me>
2025-09-30 01:46:47 +0200

    minor progress on BestTravel.cpp

--- CW/BestTravel.cpp
+++ CW/BestTravel.cpp
@@ -4,6 +4,11 @@
 
 // https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa/train/cpp
 
+
+
+
+//BestTravel.h
+
 #include <iostream>
 #include <vector>
 
@@ -12,29 +17,43 @@ using uint = std::size_t;
 
 class BestTravel {
   public:
-
-
-
-
-    static vector<vector<int>>
-    getSubsets(const int subset_size, const vector<int>& set) {
-      //TODO: implement this
-    }
-
-    static int chooseBestSum(const int t, const int k, const vector<int>& ls) {
-      static uint size = ls.size();
-      if (k > size) return -1;
-      //TODO: implement this
-    }
-
-
-
-
+    static int chooseBestSum(int t, int k, const vector<int>& ls);
+    static void testGetSubsets();
+    static void printToConsole(const vector<vector<int>>& vecvec);
+    static vector<vector<int>> getSubsets(int subset_size, const vector<int>& set);
 };
 
 
 
-void printToConsole(const vector<vector<int>>& vecvec) {
+
+//BestTravel.cpp
+
+vector<vector<int>>
+BestTravel::getSubsets(const int subset_size, const vector<int>& set) {
+  //catch invalid size input
+  if (subset_size <= 0 || subset_size > set.size()) {
+    throw std::invalid_argument("Invalid subset size");
+  }
+
+  //base case for size 1
+  if (subset_size == 1) {
+    vector<vector<int>> result;
+    for (int i: set) {
+      result.push_back({i});
+    }
+    return result;
+  }
+
+  //TODO: implement general case for size > 1
+}
+
+int BestTravel::chooseBestSum(const int t, const int k, const vector<int>& ls) {
+  static uint size = ls.size();
+  if (k > size) return -1;
+  //TODO: implement this
+}
+
+void BestTravel::printToConsole(const vector<vector<int>>& vecvec) {
   //TODO: output looks atrocious, should be improved
   std::cout << "Vector[\n";
   for (auto& vec: vecvec) {
@@ -47,6 +66,9 @@ void printToConsole(const vector<vector<int>>& vecvec) {
   std::cout << "]\n";
 }
 
-void testGetSubsets() {
-  //TODO: test getSubsets() function
+void BestTravel::testGetSubsets() {
+  printToConsole(getSubsets(1, {3, 7, 2, 5}));
 }
+
+
+
