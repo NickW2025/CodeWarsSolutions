7c7593ca24d86c8e6842d0b3cc425633ae19a3d8
Nick <NWVS25@proton.me>
2025-09-30 15:53:04 +0200

    added TheLift.cpp

---
+++ CW/TheLift.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Nick on 30/09/2025.
+//
+
+// https://www.codewars.com/kata/58905bfa1decb981da00009e/train/cpp
+
+#include <vector>
+
+using std::vector;
+
+vector<int> the_lift(const vector<vector<int>> &queues, const int capacity) {
+  return {};
+}
